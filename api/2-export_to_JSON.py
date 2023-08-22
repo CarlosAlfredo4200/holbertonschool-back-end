b626f64c182cae424c1e4d7df2758f1707eee70a
carlosalfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-08-22 14:53:36 -0500

    2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -7,13 +7,22 @@ import sys
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
+    user_response = requests.get(url + "users/{}".format(user_id))
+    user_data = user_response.json()
+    username = user_data.get("username")
+
+    todos_response = requests.get(url + "todos", params={"userId": user_id})
+    todos_data = todos_response.json()
+
+    json_filename = "{}.json".format(user_id)
+    with open(json_filename, "w") as jsonfile:
+        json.dump({
+            user_id: [{
+                "task": task.get("title"),
+                "completed": task.get("completed"),
                 "username": username
-            } for t in todos]}, jsonfile)
+            } for task in todos_data]
+        }, jsonfile, indent=4)
+
+    print("Data exported to {}".format(json_filename))

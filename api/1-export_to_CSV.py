973f492c96e08f6ab579daa85c1eb46978aa4164
carlosalfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-08-22 14:38:06 -0500

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,53 +1,18 @@
 #!/usr/bin/python3
-"""
-import module requests
-"""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    API_URL = 'https://jsonplaceholder.typicode.com'
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    user_id = argv[1]
-    response = \
-        requests.get(
-            f'{API_URL}/users/{user_id}/todos',
-            params={'_expand': 'user'}
-        )
-
-    if response.status_code == 200:
-        data = response.json()
-        name = data[0]['user']['name']
-        tasks_done = [task for task in data if task['completed']]
-        num_tasks_done = len(tasks_done)
-        num_tasks_total = len(data)
-
-        first_str = f"Employee {name} is done with tasks"
-
-        print(f"{first_str}({num_tasks_done}/{num_tasks_total}):")
-        for task in tasks_done:
-            print(f"\t {task['title']}")
- 
-        csv_filename = f"{user_id}.csv"
-        with open(csv_filename, 'w', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file)
-            csv_writer.writerow(
-                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-            for task in tasks_done:
-                csv_writer.writerow(
-                    [user_id, name, task['completed'], task['title']])
- 
-        num_lines = 0
-        with open(csv_filename, 'r') as file:
-            csv_reader = csv.reader(file)
-            for line in csv_reader:
-                if line:
-                    num_lines += 1
-
-        if num_tasks_done == num_lines - 1: 
-            print("Number of tasks in CSV: OK")
-        else:
-            print("Number of tasks in CSV: Incorrect")
-    else:
-        print(f"Error: {response.status_code}")
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]

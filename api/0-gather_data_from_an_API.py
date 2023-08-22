7f92ae756a0339ac78ed3dcf0d1977778965bcbc
carlosalfredo4200 <alfredomontoyacorreo2@gmail.com>
2023-08-22 13:39:42 -0500

    0 task

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,41 +1,33 @@
 #!/usr/bin/python3
 """
-Import the module requests
+Module documentation
+containig a lot
+of lines
 """
 import requests
-import sys
+from sys import argv
 
+if __name__ == '__main__':
+    API_URL = 'https://jsonplaceholder.typicode.com'
 
-def get_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
+    user_id = argv[1]
+    response = \
+        requests.get(
+            f'{API_URL}/users/{user_id}/todos',
+            params={'_expand': 'user'}
+        )
 
-    user_response = requests.get(f"{base_url}/users/{employee_id}")
-    user_data = user_response.json()
-    employee_name = user_data['name']
+    if response.status_code == 200:
+        data = response.json()
+        name = data[0]['user']['name']
+        tasks_done = [task for task in data if task['completed']]
+        num_tasks_done = len(tasks_done)
+        num_tasks_total = len(data)
 
-    todo_response = requests.get(f"{base_url}/todos?userId={employee_id}")
-    todo_data = todo_response.json()
+        first_str = f"Employee {name} is done with tasks"
 
-    total_tasks = len(todo_data)
-    done_tasks = [task for task in todo_data if task['completed']]
-
-    print((
-        f"Employee {employee_name} is done with tasks"
-        f"({len(done_tasks)}/{total_tasks}):"
-    ))
-    print(f"{employee_name}: name of the employee")
-    print(f"{len(done_tasks)}: number of completed tasks")
-    print(f"{total_tasks}: total number of tasks")
-
-    for task in done_tasks:
-        print(f"\t{task['title']}")
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-
-    get_employee_todo_progress(employee_id)
+        print(f"{first_str}({num_tasks_done}/{num_tasks_total}):")
+        for task in tasks_done:
+            print(f"\t {task['title']}")
+    else:
+        print(f"Error: {response.status_code}")
